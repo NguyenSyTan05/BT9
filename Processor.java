187c7a56ee266882532c0c0db30a466c56008f68
NguyenSyTan05 <sytanzxc2005@gmail.com>
2024-11-17 18:33:02 +0700

    Update Processor.java

--- Processor.java
+++ Processor.java
@@ -1,15 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.classtudent;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Administrator
- */
+
+
 public class Processor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
